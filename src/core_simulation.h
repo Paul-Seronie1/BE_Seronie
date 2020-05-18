ec79437861c2c7ffaa6a9fceab4a2f6932cc6906
Paul Séronie-Vivien <paulsronie.v@gmail.com>
2020-05-18 15:22:17 +0200

    Update core_simulation.h

--- src/core_simulation.h
+++ src/core_simulation.h
@@ -18,6 +18,8 @@ using namespace std;
 
 enum typeio {OUTPUT, INPUT2};
 enum typetest {test1, test2};
+enum typetest {test3, test4};
+
 // exceptions gerees
 enum excep {SPEED, INOUT, ADDRESS, SIZEE, EMPTY};
 class BoardException{
