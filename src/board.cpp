a4efed6143e9534b37ee3d184c27f1c4f6f839d4
TeoGeneau <57947343+TeoGeneau@users.noreply.github.com>
2020-05-20 11:33:39 +0200

    Update board.cpp

--- src/board.cpp
+++ src/board.cpp
@@ -9,15 +9,19 @@ int main(){
   AnalogSensorTemperature temperature(DELAY,TEMP);
   DigitalActuatorLED led1(DELAY);
   I2CActuatorScreen screen;
+  TensionSensor voltmetre(3, DELAY);
+  Ventilator ventilo(0, DELAY);
+
+  ventilo.setSpeed(0, 110, 1.5);
 
   // branchement des capteurs actionneurs
+  esp8266.pin(2, voltmetre);
   esp8266.pin(1,temperature);
   esp8266.pin(0,led1);
   esp8266.i2c(1,screen);
+  esp8266.pin(3, ventilo);
 
   // allumage de la carte
   esp8266.run();
   return 0;
 }
-
-
