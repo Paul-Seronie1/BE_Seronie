c7a6ed5123336513eef6740c97ed5bda415c5fbd
Paul Séronie-Vivien <paulsronie.v@gmail.com>
2020-05-20 10:54:37 +0200

    Update mydevices.cpp

--- src/mydevices.cpp
+++ src/mydevices.cpp
@@ -1,6 +1,7 @@
 #include "mydevices.h"
 #include <windows.h>
 #define sleep(x) Sleep(1000 * (x))
+
 using namespace std;
 
 //classe AnalogSensorTemperature
@@ -17,7 +18,8 @@ void AnalogSensorTemperature::run(){
   }
 }
 
-TensionSensor::TensionSensor(int v):Device(),val(v)
+//class TensionSensor
+TensionSensor::TensionSensor(int v, int t):Device(),val(v), temps(t)
 {
 }
 
@@ -25,6 +27,76 @@ void TensionSensor::run()
 {
     while(1)
     {
+        if (ptrmem!=NULL)
+        {
+            *ptrmem=val;
+        }
+        sleep(temps);
+    }
+}
+
+//class Ventilator
+Ventilator::Ventilator(bool m, int t):Device(),mode(m), temps(t)
+{
+    speed = 0;
+}
+
+int Ventilator::getSpeed()
+{
+    return speed;
+}
+
+void Ventilator::setSpeed(int s, int temp, float tension)
+{
+    if (mode) //mode manuel
+    {
+        speed = s;
+        if (speed > MAXVRPM)
+        {
+            speed = MAXVRPM;
+        }
+    }
+    else
+    {
+        if (temp < 50 && tension < 0.7)
+        {
+            speed = 0;
+        }
+        else
+        {
+            if (110*(temp-50) > (int)5392*(tension-0.7))
+            {
+                speed = 110*(temp-50);
+            }
+            else
+            {
+                speed = (int)5392*(tension-0.7);
+            }
+            if (speed > MAXVRPM)
+            {
+                speed = MAXVRPM;
+            }
+        }
+    }
+}
+
+void Ventilator::run()
+{
+    while(1)
+    {
+        if (ptrmem!=NULL)
+        {
+            *ptrmem = speed;
+        }
+        if (mode)
+        {
+            cout << "Mode manuel, speed = " << speed << endl;
+        }
+        else
+        {
+            cout << "Mode auto, speed = " << speed << endl;
+        }
+        sleep(temps);
     }
 }
 //classe DigitalActuatorLED
