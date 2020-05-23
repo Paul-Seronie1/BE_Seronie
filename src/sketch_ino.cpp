061dc013b5df277b10d1ee46c5674a7517ed6388
Paul Séronie-Vivien <paulsronie.v@gmail.com>
2020-05-23 17:15:35 +0200

    Update sketch_ino.cpp

--- src/sketch_ino.cpp
+++ src/sketch_ino.cpp
@@ -17,6 +17,7 @@ void Board::setup(){
 // on fixe les pin en entree et en sorite en fonction des capteurs/actionneurs mis sur la carte
 
   pinMode(1,INPUT2);
+<<<<<<< HEAD
 
   pinMode(2, INPUT2);
 
@@ -24,6 +25,11 @@ void Board::setup(){
 
   pinMode(3, OUTPUT);
 
+=======
+  pinMode(2, INPUT2);
+  pinMode(0,OUTPUT);
+  pinMode(3, OUTPUT);
+>>>>>>> Branch_Teo
 }
 
 
@@ -35,9 +41,13 @@ void Board::loop(){
   char buf[100];
 
   int val;
+<<<<<<< HEAD
 
   int volt;
 
+=======
+  int volt;
+>>>>>>> Branch_Teo
   static int cpt=0;
 
   static int bascule=0;
@@ -59,7 +69,13 @@ void Board::loop(){
     sprintf(buf,"tension %d",volt);
 
     Serial.println(buf);
+<<<<<<< HEAD
 
+=======
+    volt=analogRead(2);
+    sprintf(buf,"tension %d",volt);
+    Serial.println(buf);
+>>>>>>> Branch_Teo
     if(cpt%5==0){
 
         // tous les 5 fois on affiche sur l ecran la temperature
@@ -88,6 +104,9 @@ void Board::loop(){
 
   bascule=1-bascule;
 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> Branch_Teo
 }
